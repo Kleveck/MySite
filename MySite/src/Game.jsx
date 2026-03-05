2f21684b5ef752aa6179273a616d94ce10905e8d
Marco Marcelo <marco_marcelo_09@hotmail.com>
2026-03-05 11:34:47 +0000

    .

--- MySite/src/Game.jsx
+++ MySite/src/Game.jsx
@@ -3,19 +3,51 @@ import './Game.css';
 
 const Game = () => {
   return (
-    <div className="game-screen"> {/* Fundo do site (Preto) */}
-      <div className="monitor-frame"> {/* Moldura do Monitor */}
-        <div className="game-world"> {/* O ecrã cinza onde o jogo corre */}
-          
+    <div className="game-screen">
+      {" "}
+      {/* Fundo do site (Preto) */}
+      <div className="monitor-frame">
+        {" "}
+        {/* Moldura do Monitor */}
+        <div className="game-world">
+          <div className="celestial-body"></div>
           <div className="score-board">SCORE: 00000</div>
-          
-          <div className="character"></div>
 
-          <div className="ground-line">
-            <div className="obstacle tree"></div>
-            <div className="obstacle mountain"></div>
+          {/* GRUPO 1: O Bosque (4 ou 5 árvores) */}
+          <div className="bg-group forest-group">
+            <div className="tree-container size-sm">
+              {" "}
+              <div className="tree-top"></div>
+              <div className="tree-trunk"></div>{" "}
+            </div>
+            <div className="tree-container size-md">
+              {" "}
+              <div className="tree-top"></div>
+              <div className="tree-trunk"></div>{" "}
+            </div>
+            <div className="tree-container size-lg">
+              {" "}
+              <div className="tree-top"></div>
+              <div className="tree-trunk"></div>{" "}
+            </div>
+            <div className="tree-container size-sm">
+              {" "}
+              <div className="tree-top"></div>
+              <div className="tree-trunk"></div>{" "}
+            </div>
           </div>
 
+          {/* GRUPO 2: Montanha + Espaço + Árvore Isolada */}
+          <div className="bg-group mountain-island">
+            <div className="mountain"></div>
+            <div className="tree-container size-md isolated-tree">
+              <div className="tree-top"></div>
+              <div className="tree-trunk"></div>
+            </div>
+          </div>
+
+          <div className="character"></div>
+          <div className="ground-line"></div>
         </div>
       </div>
       <div className="scroll-hint">Scroll down for CV</div>
